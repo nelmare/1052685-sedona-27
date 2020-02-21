69076675dff242444414f3a778c72ea578462338
nelmare <50352500+nelmare@users.noreply.github.com>
2020-02-21 23:48:42 +0300

    Добавляет js форме

--- js/form.js
+++ js/form.js
@@ -1,6 +1,5 @@
 var button = document.querySelector(".brown-button");
 var form = document.querySelector(".index-form");
-var close = document.querySelector(".form-close");
 var arrival = form.querySelector("[name=arrival-date]");
 var departure = form.querySelector("[name=departure-date]");
 var adults = form.querySelector("[name=adult-guests]");
@@ -10,46 +9,42 @@ var isStorageSupport = true;
 var storage = "";
 
 try {
-  storage = localStorage.getItem("adult-guests");
-  storage = localStorage.getItem("children-guests");
+  storageAdults = localStorage.getItem("adult-guests");
+  storageChildren = localStorage.getItem("children-guests");
 } catch (err) {
   isStorageSupport = false;
 }
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
-  form.classList.add("form-show");
-  if (storage) {
-    adults.value = storage;
-    children.value = storage;
-  }
-
+  form.classList.toggle("form-show");
+  form.classList.remove("form-error");
+  if (isStorageSupport) {
+    adults.value = storageAdults;
+    children.value = storageChildren;
+    }
   arrival.focus();
 });
 
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  form.classList.remove("form-show");
-
-  form.classList.add("form-show");
-});
-
 form.addEventListener("submit", function (evt) {
-  if (!arrival.value || !departure.value || !adults.value || !children.value) {
+  if (!arrival.value || !departure.value || !adults.value) {
     evt.preventDefault();
-    console.log("Нужно ввести данные в форму");
+    form.classList.remove("form-error");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("form-error");
   } else {
     localStorage.setItem("adult-guests", adults.value);
     localStorage.setItem("children-guests", children.value);
   }
 });
 
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (form.classList.contains("modal-show")) {
-        form.classList.remove("modal-show");
-      }
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (form.classList.contains("modal-show")) {
+      form.classList.remove("modal-show");
+      form.classList.remove("form-error");
     }
+  }
 });
 
